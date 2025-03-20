cfb17a45106e0cf90f8c02853d6a4ab4cc434770
amyheather <a.heather2@exeter.ac.uk>
2025-03-20 13:37:16 +0000

    feat(app): add home page for multi-page web app

---
+++ Home.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.title("LLM-Generated Simulation Apps")
+st.markdown(
+    """
+**Discrete-event simulation (DES) model web applications generated using Large
+Language Models (LLMs).**"""
+)
+
+st.divider()
+st.markdown(
+    """
+This application is complementary to:
+
+> Thomas Monks, Alison Harper, Amy Heather, and Navonil Mustafee. **Using Large
+Language Models to support researchers reproduce and reuse unpublished health
+care discrete-event simulation computer models: a feasibility and pilot study
+in Python**. https://github.com/pythonhealthdatascience/llm_simpy.
+
+It deploys the streamlit applications generated using Perplexity as a single
+app on Streamlit Community Cloud.
+            
+Use the sidebar to navigate to each app.
+"""
+)
+
+st.divider()
+st.markdown(
+    """
+Source code: https://github.com/pythonhealthdatascience/llm_simpy_models."""
+)

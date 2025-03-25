3a2c5ecd781b320aa98ffa440c5c2ebeae1ed47c
amyheather <a.heather2@exeter.ac.uk>
2025-03-25 14:15:01 +0000

    feat(home): add info box explaining st.spinner() absence with stlite

--- Home.py
+++ Home.py
@@ -25,6 +25,16 @@ Use the sidebar to navigate to each app.
 )
 
 st.divider()
+st.info("""
+The apps use `st.spinner()` to show a spinning icon when the model is running,
+but this does not work with `stlite`. Hence, it may appear like nothing is
+happening, when the model is actually running behind the scenes (once you
+click "simulate"). This is mainly relevant to the stroke model which takes a
+little longer to run, but you can speed it up by reducing the number of
+replications.
+""", icon="ℹ️")
+
+st.divider()
 st.markdown(
     """
 Source code: https://github.com/pythonhealthdatascience/llm_simpy_models."""
